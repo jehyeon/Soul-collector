99e4237fae4f9aebf88ab573dc747a17383d999e
jehyeon <hyeon.rot@gmail.com>
2021-12-19 14:16:00 +0900

    add temporary follow enemy to player, attack

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float playerSpeed = 10f;
+    public float hp = 100f;
     
     private bool isMove;
     private Vector3 destinationPos;
     
+    private bool isTarget;
+    private GameObject targetEnemy;
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -17,14 +21,27 @@ public class PlayerController : MonoBehaviour
 
             if (Physics.Raycast(ray, out RaycastHit raycastHit))
             {
+                // 땅 클릭하면 그 위치로 이동
                 if (raycastHit.collider.CompareTag("Ground"))
                 {
                     SetDestination(raycastHit.point + new Vector3(0, 1, 0));
+                }
+
+                // 적 클릭하면 target 설정
+                if (raycastHit.collider.CompareTag("Enemy"))
+                {
+                    targetEnemy = raycastHit.collider.gameObject;
+                    isTarget = true;
                 }
             }
         }
 
         Move();
+
+        if (isTarget)
+        {
+            Attack();
+        }
     }
 
     private void Move()
@@ -48,5 +65,24 @@ public class PlayerController : MonoBehaviour
     {
         destinationPos = pos;
         isMove = true;
+    }
+
+    private void Attack()
+    {
+        if (Vector3.Distance(targetEnemy.transform.position, this.transform.position) <= 1.1f)
+        {
+            targetEnemy.GetComponent<EnemyController>().Hit(Time.deltaTime);
+        }
+    }
+
+    public void Kill()
+    {
+        isTarget = false;
+        targetEnemy = null;
+    }
+
+    public void Hit(float damage)
+    {
+        hp -= damage;
     }
 }
