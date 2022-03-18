cc1237e53bf84a155d686a7917c4b2c0f5ce9876
jehyeon <hyeon.rot@gmail.com>
2022-03-18 17:36:39 +0900

    fix: fix issue that item tooltip went off screen

--- Assets/Scripts/UI/ItemDetail.cs
+++ Assets/Scripts/UI/ItemDetail.cs
@@ -15,6 +15,8 @@ public class ItemDetail : MonoBehaviour
 
     public void Open(Item selectedItem, Vector3 position)
     {
+        float width = GetComponent<RectTransform>().rect.width;
+        float height = GetComponent<RectTransform>().rect.height;
         // 슬롯 정보 업데이트
         slot.Set(selectedItem);
         itemName.text = selectedItem.ItemName;
@@ -23,10 +25,22 @@ public class ItemDetail : MonoBehaviour
 
         // 위치 조정
         position += new Vector3(
-            GetComponent<RectTransform>().rect.width * 0.5f,
-            GetComponent<RectTransform>().rect.height * 0.5f * -1f,
+            width * 0.5f,
+            height * 0.5f * -1f,
             0
         );
+
+        // 아이템 툴팁이 화면 밖으로 나가는 경우
+        // 위치를 화면 안으로 밀어줌
+        if (position.x + width * 0.5f > Screen.width)
+        {
+            position.x = Screen.width - width * 0.5f;
+        }
+        if (position.y + height * -.5f < 0)
+        {
+            position.y = height * 0.5f;
+        }
+
         this.transform.position = position;
         
         this.gameObject.SetActive(true);
