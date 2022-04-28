3263bf80cb1e840c852a8d0286c653f07c1d050f
jehyeon <hyeon.rot@gmail.com>
2022-04-28 22:44:29 +0900

    try sqlite db

---
+++ server/init.py
@@ -0,0 +1,55 @@
+import sqlite3
+import os
+
+def main():
+    print('Reset? (say "Yes")')
+    reset = input('=> ')
+
+    if not reset == 'Yes':
+        pass
+    
+    os.remove('./database.sqlite')
+
+    conn = sqlite3.connect('database.sqlite')
+    
+    # Make users table
+    conn.execute(
+        '''
+        CREATE TABLE users (
+            id TEXT,
+            PRIMARY KEY(id)
+        )
+        '''
+    )
+
+    # Make auction table
+    conn.execute(
+        '''
+        CREATE TABLE auction (
+            userId TEXT,
+            itemId INTEGER,
+            price INTEGER,
+            time TEXT,
+            rank INTEGER,
+            type INTEGER,
+            FOREIGN KEY (userId) REFERENCES users (id)
+        )
+        '''
+    )
+    # Make push table
+    conn.execute(
+        '''
+        CREATE TABLE push (
+            userId TEXT,
+            itemId INTEGER,
+            time TEXT,
+            FOREIGN KEY (userId) REFERENCES users (id)
+        )
+        '''
+    )
+
+    conn.commit()
+    conn.close()
+
+if __name__ == '__main__':
+    main()
