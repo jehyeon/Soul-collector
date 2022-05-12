40ebad52129c78f1c7337b8d95a68214ca640d19
jehyeon <hyeon.rot@gmail.com>
2022-05-12 22:27:13 +0900

    add enemies

---
+++ Assets/Imported/_InfinityPBR - Troll/Scripts/SFB_TrollDemo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFB_TrollDemo : MonoBehaviour {
+
+	public void Locomotion(float newValue)
+    {
+        GetComponent<Animator>().SetFloat("Locomotion", newValue);
+    }
+}
